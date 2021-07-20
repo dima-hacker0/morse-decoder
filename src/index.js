4c1d89ffcfd0981f79dddb8c8832dcf00b4ddddd
Dima Vaitsiachovski <boretskiy70@bk.ru>
2021-07-21 01:40:37 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -38,7 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    perPoint = 0
+    let arrCifr = [];
+    for(let i = 0; i < expr.length; i += 10) {
+        let str = expr.substr(i, 10);
+        arrCifr[perPoint] = str;
+        perPoint++;
+    }
+    let arrPoint = [];
+    let shet = 0;
+    for(let i = 0; i < expr.length / 10; i++){
+        arrPoint[i] = '';
+        if(arrCifr[i] == "**********"){
+            arrPoint[i] = " ";
+            continue;
+        }
+        for(let j = 0; j < 5; j++) {
+            let temp = arrCifr[i].substr(shet, 2);
+            if(temp == '10') {
+                let temp1 = arrPoint[i] + '.';
+                arrPoint[i] = temp1;
+                
+            }
+            if(temp == '11') {
+                let temp2 = arrPoint[i] + '-';
+                arrPoint[i] = temp2;
+            }
+            shet += 2;
+        }
+        shet = 0;
+    }
+    arrBukv = [];
+    for(let i = 0; i < expr.length / 10; i++) {
+        if(arrPoint[i] == ' ') {
+            arrBukv[i] = ' ';
+            continue;
+        }
+        arrBukv[i] = MORSE_TABLE[arrPoint[i]];
+    }
+    return arrBukv.join('');
 }
 
 module.exports = {
